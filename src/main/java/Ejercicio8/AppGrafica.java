34a83f39a8e7b5fffd087dcfcb62a9f95901d2c3
sansung14 <78426816+sansung14@users.noreply.github.com>
2021-03-02 10:07:05 +0100

    Ej8-añadido el keypressed

--- src/main/java/Ejercicio8/AppGrafica.java
+++ src/main/java/Ejercicio8/AppGrafica.java
@@ -6,19 +6,25 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
 import java.text.DecimalFormat;
 import java.awt.event.ActionEvent;
 
 public class AppGrafica extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtAsdf;
-	private JTextField textField;
+	private static JTextField txtAsdf;
+	private static JTextField textField;
+	protected static JButton btnNewButton;
 	private static DecimalFormat df = new DecimalFormat("0.0000");
 
 	/**
@@ -29,6 +35,7 @@ public class AppGrafica extends JFrame {
 			public void run() {
 				try {
 					AppGrafica frame = new AppGrafica();
+					teclado();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -47,80 +54,116 @@ public class AppGrafica extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
-		
-		JButton btnNewButton = new JButton("Euros a ptas");
+
+		btnNewButton = new JButton("Euros a ptas");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		btnNewButton.setBounds(63, 67, 120, 23);
 		contentPane.add(btnNewButton);
-		
+
 		JLabel lblNewLabel = new JLabel("Cantidad a convertir:");
 		lblNewLabel.setBounds(24, 24, 120, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		txtAsdf = new JTextField();
 		txtAsdf.setText("56");
 		txtAsdf.setBounds(151, 21, 86, 20);
 		contentPane.add(txtAsdf);
 		txtAsdf.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Resultado:");
 		lblNewLabel_1.setBounds(247, 24, 72, 14);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JButton btnNewButton_1 = new JButton("Cambiar");
 		btnNewButton_1.setBounds(193, 67, 89, 23);
 		contentPane.add(btnNewButton_1);
-		
+
 		textField = new JTextField();
 		textField.setText("234");
 		textField.setEditable(false);
 		textField.setBounds(316, 21, 86, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		JButton btnNewButton_2 = new JButton("Borrar");
 		btnNewButton_2.setBounds(287, 67, 89, 23);
 		contentPane.add(btnNewButton_2);
-		
-		
+
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
-					if(btnNewButton.getText().equals("Euros a ptas")) {
-						double resultado = Double.parseDouble(txtAsdf.getText())*166.386;
+					if (btnNewButton.getText().equals("Euros a ptas")) {
+						double resultado = Double.parseDouble(txtAsdf.getText()) * 166.386;
 						textField.setText(df.format(resultado));
-					}else {
-						double resultado = Double.parseDouble(txtAsdf.getText())/166.386;
+					} else {
+						double resultado = Double.parseDouble(txtAsdf.getText()) / 166.386;
 						textField.setText(df.format(resultado));
 					}
-				}catch (NumberFormatException er) {
-	                JOptionPane.showMessageDialog(null, "¡Error no has introducido un numero o as puesto una ','!");                
+				} catch (NumberFormatException er) {
+					JOptionPane.showMessageDialog(null, "¡Error no has introducido un numero o as puesto una ','!");
 				}
-				
+
 			}
 		});
-		
+
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(btnNewButton.getText().equals("Euros a ptas")) {
+				if (btnNewButton.getText().equals("Euros a ptas")) {
 					btnNewButton.setText("Ptas a Euros");
-				}else {
+				} else {
 					btnNewButton.setText("Euros a ptas");
 				}
 			}
 		});
-		
+
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				txtAsdf.setText("");
 				textField.setText("");
 			}
 		});
-		
-		
+
 	}
+
+	public static void teclado() {
+		txtAsdf.addKeyListener(new KeyAdapter() {
+
+			
+		
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				try {
+					int key = e.getKeyCode();
+					if (key == KeyEvent.VK_ENTER) {
+						if (btnNewButton.getText().equals("Euros a ptas")) {
+							double resultado = Double.parseDouble(txtAsdf.getText()) * 166.386;
+							textField.setText(df.format(resultado));
+						} else {
+							double resultado = Double.parseDouble(txtAsdf.getText()) / 166.386;
+							textField.setText(df.format(resultado));
+						}
+					}
+				} catch (NumberFormatException er) {
+					JOptionPane.showMessageDialog(null, "¡Error no has introducido un numero o as puesto una ',' !");
+				}
+			}
+		});
+
+}
 
 }
